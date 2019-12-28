d02203399b4445132e1de18df20213c9da8536d4
asmirnov69 <asmirnov69@users.noreply.github.com>
2019-12-28 03:55:44 +0000

    added HierarchyView.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,8 @@
 import React from "react";
 import SHACLEditor from './SHACLEditor.js';
 import SHACLClassEditorDialog from './SHACLClassEditorDialog.js';
+import HierarchyView from './HierarchyView.js';
 import ReactDataSheet from 'react-datasheet';
-
-//
-// treeview example
-// https://material-ui.com/components/tree-view/
-// https://github.com/mui-org/material-ui/blob/master/docs/src/pages/components/tree-view/FileSystemNavigator.js
-//
-import { makeStyles } from '@material-ui/core/styles';
-import TreeView from '@material-ui/lab/TreeView';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import TreeItem from '@material-ui/lab/TreeItem';
-
-const useStyles = makeStyles({
-  root: {
-    height: 216,
-    flexGrow: 1,
-    maxWidth: 400,
-  },
-});
 
 export default class App extends React.Component {
     constructor(props) {
@@ -36,29 +18,11 @@ export default class App extends React.Component {
     }
 
     render() {
-	const classes = useStyles;
 	return (<div style={{display: "grid", width: "100%", height: "100%",
 			     gridTemplateColumns: "180px auto",
 			     gridTemplateRows: "auto 200px"}}>
 		<div>
-		 <TreeView className={classes.root}
-		  defaultCollapseIcon={<ExpandMoreIcon />}
-		  defaultExpandIcon={<ChevronRightIcon />}
-		>
-		  <TreeItem nodeId="1" label="Applications">
-		   <TreeItem nodeId="2" label="Calendar" />
-		   <TreeItem nodeId="3" label="Chrome" />
-		   <TreeItem nodeId="4" label="Webstorm" />
-		  </TreeItem>
-		  <TreeItem nodeId="5" label="Documents">
-		   <TreeItem nodeId="6" label="Material-UI">
-		    <TreeItem nodeId="7" label="src">
-		     <TreeItem nodeId="8" label="index.js" />
-		     <TreeItem nodeId="9" label="tree-view.js" />
-		    </TreeItem>
-		   </TreeItem>
-		  </TreeItem>
-		 </TreeView>
+		  <HierarchyView top_app={this}/>
 		</div>
 		<div style={{backgroundColor: "cyan"}}>
 		<SHACLEditor top_app={this} db_uri_scheme="testdb"/>
