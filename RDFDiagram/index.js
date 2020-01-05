4707210e70ebf50fa9b0e1ec09b3e9f45fd5087f
asmirnov69 <asmirnov69@users.noreply.github.com>
2020-01-05 08:05:39 +0000

    added RDFDiagram

---
+++ RDFDiagram/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RDFDiagram from './RDFDiagram.js';
+
+ReactDOM.render(<RDFDiagram/>, document.getElementById('root'));
---
+++ RDFDiagram/rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import builtins from 'rollup-plugin-node-builtins';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+    input: 'index.js',
+    output: {
+	file: 'public/bundle.js',
+	format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+	sourcemap: true,
+    },
+    plugins: [
+	builtins({preferBuiltins: true}),
+	babel({babelrc: false, presets: ['@babel/react']}),
+	replace({'process.env.NODE_ENV': JSON.stringify('production'),}), // to fix react imports
+	resolve(), // tells Rollup how to find date-fns in node_modules
+	commonjs({namedExports: {		
+		'node_modules/react-is/index.js': ['ForwardRef'],
+		'node_modules/react/index.js': ['createContext', 'createElement', 'Children', 'PureComponent', 'Component', 'createRef', 'Fragment', 'isValidElement', 'cloneElement'],
+		'node_modules/react-dom/index.js': ['unstable_batchedUpdates', 'findDOMNode'],
+		'node_modules/prop-types/index.js': ['shape', 'instanceOf', 'node', 'object', 'string', 'func', 'bool', 'oneOfType', 'number', 'arrayOf', 'oneOf', 'any', 'elementType'],
+	    }}), // converts date-fns to ES modules
+	//production && terser() // minify, but only in production
+    ]
+};
