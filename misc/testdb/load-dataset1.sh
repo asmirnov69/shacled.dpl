04179bc6d2b6d8c7abb40e5e53202c628fe68fe9
asmirnov69 <asmirnov69@users.noreply.github.com>
2020-04-15 05:31:22 +0000

    added more adhoc scripts

---
+++ misc/testdb/load-dataset1.sh
@@ -0,0 +1,2 @@
+~/Downloads/apache-jena-fuseki-3.13.1/bin/s-put http://localhost:3030/testdb1 testdb:shacl-defs shacl-defs.ttl
+~/Downloads/apache-jena-fuseki-3.13.1/bin/s-put http://localhost:3030/testdb1 default data.ttl
